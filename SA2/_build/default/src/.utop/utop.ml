ba515ad5d37e899def32da56de40ca193388c43a
John Cinquegrana <cs496@LAPTOP-HPFDDR70.localdomain>
2020-05-05 19:58:59 -0400

    NewObject case finished

---
+++ SA2/_build/default/src/.utop/utop.ml
@@ -0,0 +1,24 @@
+Clflags.include_dirs :=
+  [ "/home/cs496/.opam/system/lib/bytes"
+  ; "/home/cs496/.opam/system/lib/camomile"
+  ; "/home/cs496/.opam/system/lib/camomile/default_config"
+  ; "/home/cs496/.opam/system/lib/camomile/dyn"
+  ; "/home/cs496/.opam/system/lib/camomile/lib_default"
+  ; "/home/cs496/.opam/system/lib/camomile/library"
+  ; "/home/cs496/.opam/system/lib/findlib"
+  ; "/home/cs496/.opam/system/lib/lambda-term"
+  ; "/home/cs496/.opam/system/lib/lwt"
+  ; "/home/cs496/.opam/system/lib/lwt/unix"
+  ; "/home/cs496/.opam/system/lib/lwt_log"
+  ; "/home/cs496/.opam/system/lib/lwt_log/core"
+  ; "/home/cs496/.opam/system/lib/lwt_react"
+  ; "/home/cs496/.opam/system/lib/react"
+  ; "/home/cs496/.opam/system/lib/result"
+  ; "/home/cs496/.opam/system/lib/utop"
+  ; "/home/cs496/.opam/system/lib/zed"
+  ; "/usr/lib/ocaml/compiler-libs"
+  ; "/usr/lib/ocaml/threads"
+  ; "/mnt/d/Stevens/2020Spring/OCaml/midterm/SA2/_build/default/src/.sool.objs"
+  ];
+
+UTop_main.main ();
